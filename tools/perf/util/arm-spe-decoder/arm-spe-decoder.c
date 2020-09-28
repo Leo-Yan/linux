12866725d857efa6a0014bff98d69e43a65037ad
Leo Yan <leo.yan@linaro.org>
2020-09-28 05:34:57 +0100

    perf arm-spe: Save context ID in record
    This patch is to save context ID in record, this will be used to set TID
    for samples.
    
    Signed-off-by: Leo Yan <leo.yan@linaro.org>

--- tools/perf/util/arm-spe-decoder/arm-spe-decoder.c
+++ tools/perf/util/arm-spe-decoder/arm-spe-decoder.c
@@ -168,6 +168,7 @@ static int arm_spe_read_record(struct arm_spe_decoder *decoder)
 		case ARM_SPE_COUNTER:
 			break;
 		case ARM_SPE_CONTEXT:
+			decoder->record.context_id = payload;
 			break;
 		case ARM_SPE_OP_TYPE:
 			/*
