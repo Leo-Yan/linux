f1cb3f66e5c0ab302917ee3b9d60f19e5a75ea30
Leo Yan <leo.yan@linaro.org>
2020-11-06 07:06:05 +0000

    perf c2c: Support memory event PERF_MEM_EVENTS__LOAD_STORE
    When user doesn't specify event name, perf c2c tool enables both the
    load and store events, and this leads to failure for opening the
    duplicate PMU device for AUX trace.
    
    After the memory event PERF_MEM_EVENTS__LOAD_STORE is introduced, when
    the user doesn't specify event name, this patch converts the required
    operation to PERF_MEM_EVENTS__LOAD_STORE if the arch supports it.
    Otherwise, the tool still rolls back to enable events
    PERF_MEM_EVENTS__LOAD and PERF_MEM_EVENTS__STORE.
    
    Signed-off-by: Leo Yan <leo.yan@linaro.org>

--- tools/perf/builtin-c2c.c
+++ tools/perf/builtin-c2c.c
@@ -2895,11 +2895,16 @@ static int perf_c2c__record(int argc, const char **argv)
 	rec_argv[i++] = "record";
 
 	if (!event_set) {
-		e = perf_mem_events__ptr(PERF_MEM_EVENTS__LOAD);
-		e->record = true;
+		e = perf_mem_events__ptr(PERF_MEM_EVENTS__LOAD_STORE);
+		if (e->tag) {
+			e->record = true;
+		} else {
+			e = perf_mem_events__ptr(PERF_MEM_EVENTS__LOAD);
+			e->record = true;
 
-		e = perf_mem_events__ptr(PERF_MEM_EVENTS__STORE);
-		e->record = true;
+			e = perf_mem_events__ptr(PERF_MEM_EVENTS__STORE);
+			e->record = true;
+		}
 	}
 
 	e = perf_mem_events__ptr(PERF_MEM_EVENTS__LOAD);
