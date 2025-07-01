cba494b146eb6f07d16f1e34569cacc953576e02
Leo Yan <leo.yan@arm.com>
2025-07-01 15:53:52 +0100

    coresight: Move CPU hotplug callbacks to core layer
    This commit moves CPU hotplug callbacks from ETMv4 driver to core layer.
    The motivation is the core layer can control all components on an
    activated path rather but not only managing tracer in ETMv4 driver.
    
    The perf event layer will disable CoreSight PMU event 'cs_etm' when
    hotplug off a CPU.  That means a perf mode will be always converted to
    disabled mode in CPU hotplug.  Arm CoreSight CPU hotplug callbacks only
    need to handle the Sysfs mode and ignore the perf mode.
    
    The core layer disables and releases the active path when hotplug-off a
    CPU.  When hotplug-in a CPU, it does not re-enable the path, in this
    case, users need to re-enable the trace via Sysfs interface.
    
    Tested-by: James Clark <james.clark@linaro.org>
    Reviewed-by: Yeoreum Yun <yeoreum.yun@arm.com>
    Reviewed-by: James Clark <james.clark@linaro.org>
    Signed-off-by: Leo Yan <leo.yan@arm.com>

--- drivers/hwtracing/coresight/coresight-core.c
+++ drivers/hwtracing/coresight/coresight-core.c
@@ -1847,13 +1847,47 @@ static struct notifier_block coresight_cpu_pm_nb = {
 	.notifier_call = coresight_cpu_pm_notify,
 };
 
+static int coresight_dying_cpu(unsigned int cpu)
+{
+	struct coresight_path *path = coresight_get_percpu_local_path();
+	struct coresight_device *source = coresight_get_source(path);
+
+	if (!path || !source)
+		return 0;
+
+	/*
+	 * The perf event layer will disable PMU events in the CPU hotplug.
+	 * CoreSight driver should never handle the CS_MODE_PERF case.
+	 */
+	if (coresight_get_mode(source) != CS_MODE_SYSFS)
+		return 0;
+
+	coresight_disable_source(source, NULL);
+	coresight_disable_path(path);
+	coresight_release_path(path);
+	return 0;
+}
+
 static int __init coresight_pm_setup(void)
 {
-	return cpu_pm_register_notifier(&coresight_cpu_pm_nb);
+	int ret;
+
+	ret = cpu_pm_register_notifier(&coresight_cpu_pm_nb);
+	if (ret)
+		return ret;
+
+	ret = cpuhp_setup_state_nocalls(CPUHP_AP_ARM_CORESIGHT_STARTING,
+					"arm/coresight-core:starting",
+					NULL, coresight_dying_cpu);
+	if (ret)
+		cpu_pm_unregister_notifier(&coresight_cpu_pm_nb);
+
+	return ret;
 }
 
 static void coresight_pm_cleanup(void)
 {
+	cpuhp_remove_state_nocalls(CPUHP_AP_ARM_CORESIGHT_STARTING);
 	cpu_pm_unregister_notifier(&coresight_cpu_pm_nb);
 }
 
