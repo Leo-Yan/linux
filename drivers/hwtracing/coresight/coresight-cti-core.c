1c8476151d8cd14b1ebda3d4bdf1303266f0ccda
Leo Yan <leo.yan@arm.com>
2025-07-01 15:53:43 +0100

    coresight: cti: Fix race condition by using device mode
    A CTI device can be enabled as a helper in a CoreSight path or directly
    via the SysFS knob. Currently, the driver lacks protection when multiple
    flows contend for access to the same CTI device.
    
    Use the device mode to mark the CTI as in use, and reset it to the
    DISABLED state when the device is no longer needed. Any conflict will
    cause the attempt to acquire the mode to fail, ensuring the device can
    only be enabled exclusively.
    
    Fixes: 1b5b1646e63d ("coresight: Fix CTI module refcount leak by making it a helper device")
    Tested-by: James Clark <james.clark@linaro.org>
    Signed-off-by: Leo Yan <leo.yan@arm.com>

--- drivers/hwtracing/coresight/coresight-cti-core.c
+++ drivers/hwtracing/coresight/coresight-cti-core.c
@@ -87,24 +87,36 @@ void cti_write_all_hw_regs(struct cti_drvdata *drvdata)
 }
 
 /* write regs to hardware and enable */
-static int cti_enable_hw(struct cti_drvdata *drvdata)
+static int cti_enable_hw(struct cti_drvdata *drvdata, enum cs_mode mode)
 {
+	struct coresight_device	*csdev = drvdata->csdev;
 	struct cti_config *config = &drvdata->config;
+	enum cs_mode curr_mode;
 	int rc;
 
 	guard(raw_spinlock)(&drvdata->spinlock);
+
+	curr_mode = coresight_get_mode(csdev);
+
+	/* The device has been configured with an incompatible mode */
+	if (curr_mode != CS_MODE_DISABLED && curr_mode != mode)
+		return -EBUSY;
 
 	/* no need to do anything if enabled or unpowered*/
 	if (config->hw_enabled || !config->hw_powered)
 		goto cti_state_unchanged;
 
+	if (drvdata->config.enable_req_count)
+		goto cti_state_unchanged;
+
 	/* claim the device */
-	rc = coresight_claim_device(drvdata->csdev);
+	rc = coresight_claim_device(csdev);
 	if (rc)
 		return rc;
 
 	cti_write_all_hw_regs(drvdata);
 
+	coresight_set_mode(csdev, mode);
 	config->hw_enabled = true;
 
 cti_state_unchanged:
@@ -156,6 +168,8 @@ static int cti_disable_hw(struct cti_drvdata *drvdata)
 	/* check refcount - disable on 0 */
 	if (--drvdata->config.enable_req_count > 0)
 		goto cti_not_disabled;
+
+	coresight_set_mode(csdev, CS_MODE_DISABLED);
 
 	/* no need to do anything if disabled or cpu unpowered */
 	if (!config->hw_enabled || !config->hw_powered)
@@ -796,7 +810,7 @@ int cti_enable(struct coresight_device *csdev, enum cs_mode mode,
 {
 	struct cti_drvdata *drvdata = csdev_to_cti_drvdata(csdev);
 
-	return cti_enable_hw(drvdata);
+	return cti_enable_hw(drvdata, mode);
 }
 
 int cti_disable(struct coresight_device *csdev, struct coresight_path *path)
