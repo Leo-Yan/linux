0b9c9d1e516b2bfd6b30f73aab9dfd16b9cad7a8
Leo Yan <leo.yan@arm.com>
2025-09-05 10:42:23 +0100

    perf sort: Improve hist for simd flags
    Signed-off-by: Leo Yan <leo.yan@arm.com>

--- tools/perf/util/sort.c
+++ tools/perf/util/sort.c
@@ -195,6 +195,10 @@ static const char *hist_entry__get_simd_name(struct simd_flags *simd_flags)
 
 	if (arch & SIMD_OP_FLAGS_ARCH_SVE)
 		return "SVE";
+	if (arch & SIMD_OP_FLAGS_ARCH_SME)
+		return "SME";
+	if (arch & SIMD_OP_FLAGS_ARCH_ASE)
+		return "ASE";
 	else
 		return "n/a";
 }
@@ -203,6 +207,7 @@ static int hist_entry__simd_snprintf(struct hist_entry *he, char *bf,
 				     size_t size, unsigned int width __maybe_unused)
 {
 	const char *name;
+	const char *pred_str = ".";
 
 	if (!he->simd_flags.arch)
 		return repsep_snprintf(bf, size, "");
@@ -210,11 +215,15 @@ static int hist_entry__simd_snprintf(struct hist_entry *he, char *bf,
 	name = hist_entry__get_simd_name(&he->simd_flags);
 
 	if (he->simd_flags.pred & SIMD_OP_FLAGS_PRED_EMPTY)
-		return repsep_snprintf(bf, size, "[e] %s", name);
+		pred_str = "e";
 	else if (he->simd_flags.pred & SIMD_OP_FLAGS_PRED_PARTIAL)
-		return repsep_snprintf(bf, size, "[p] %s", name);
+		pred_str = "p";
+	else if (he->simd_flags.pred & SIMD_OP_FLAGS_PRED_DISABLED)
+		pred_str = "d";
+	else if (he->simd_flags.pred & SIMD_OP_FLAGS_PRED_FULL)
+		pred_str = "f";
 
-	return repsep_snprintf(bf, size, "[.] %s", name);
+	return repsep_snprintf(bf, size, "[%s] %s", pred_str, name);
 }
 
 struct sort_entry sort_simd = {
