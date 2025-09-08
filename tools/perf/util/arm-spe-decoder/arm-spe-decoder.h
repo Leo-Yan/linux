3f1fb05330bf768ac6d183270b334a18386680df
Leo Yan <leo.yan@arm.com>
2025-09-08 10:45:29 +0100

    perf arm_spe: Introduce data processing macro for SVE operations
    Introduce the ARM_SPE_OP_DP (data processing) macro as associated
    information for SVE operations. For SVE register access, only
    ARM_SPE_OP_SVE is set; for SVE data processing, both ARM_SPE_OP_SVE and
    ARM_SPE_OP_DP are set together.
    
    Signed-off-by: Leo Yan <leo.yan@arm.com>

--- tools/perf/util/arm-spe-decoder/arm-spe-decoder.h
+++ tools/perf/util/arm-spe-decoder/arm-spe-decoder.h
@@ -42,8 +42,7 @@ enum arm_spe_op_type {
 	ARM_SPE_OP_UNSPEC_REG		= 1 << 9,
 	ARM_SPE_OP_NV_SYSREG		= 1 << 10,
 	ARM_SPE_OP_SIMD_FP		= 1 << 11,
-	ARM_SPE_OP_SVE_OTHER		= 1 << 12,
-	ARM_SPE_OP_SVE_LDST		= 1 << 13,
+	ARM_SPE_OP_SVE			= 1 << 12,
 
 	/* Assisted information for memory / SIMD */
 	ARM_SPE_OP_LD			= 1 << 20,
@@ -51,6 +50,7 @@ enum arm_spe_op_type {
 	ARM_SPE_OP_ATOMIC		= 1 << 22,
 	ARM_SPE_OP_EXCL			= 1 << 23,
 	ARM_SPE_OP_AR			= 1 << 24,
+	ARM_SPE_OP_DP			= 1 << 25,	/* Data processing */
 
 	/* Second level operation type for BRANCH_ERET */
 	ARM_SPE_OP_BR_COND		= 1 << 8,
