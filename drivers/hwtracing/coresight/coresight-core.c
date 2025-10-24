226ac77baf693e5311d4a0d82723665611e58f39
Leo Yan <leo.yan@arm.com>
2025-10-24 11:01:33 +0100

    coresight: Save active path for system tracers
    Store the active path in the source device structure for system
    tracers (e.g. STM).  Ensure the path pointer is cleared when the
    source is disabled.
    
    Tested-by: James Clark <james.clark@linaro.org>
    Reviewed-by: Yeoreum Yun <yeoreum.yun@arm.com>
    Reviewed-by: James Clark <james.clark@linaro.org>
    Signed-off-by: Leo Yan <leo.yan@arm.com>

--- drivers/hwtracing/coresight/coresight-core.c
+++ drivers/hwtracing/coresight/coresight-core.c
@@ -447,10 +447,24 @@ int coresight_enable_source(struct coresight_device *csdev,
 			    struct perf_event *event, enum cs_mode mode,
 			    struct coresight_path *path)
 {
+	int ret;
+
 	if (!coresight_is_device_source(csdev))
 		return -EINVAL;
 
-	return source_ops(csdev)->enable(csdev, event, mode, path);
+	ret = source_ops(csdev)->enable(csdev, event, mode, path);
+	if (ret)
+		return ret;
+
+	/*
+	 * The per-CPU source has updated its path pointer in the enable()
+	 * callback, ensuring synchronization on the target CPU. Set the
+	 * path pointer here for non per-CPU sources.
+	 */
+	if (!coresight_is_percpu_source(csdev))
+		csdev->path = path;
+
+	return 0;
 }
 
 void coresight_disable_source(struct coresight_device *csdev, void *data)
@@ -459,6 +473,9 @@ void coresight_disable_source(struct coresight_device *csdev, void *data)
 		return;
 
 	source_ops(csdev)->disable(csdev, data);
+
+	if (!coresight_is_percpu_source(csdev))
+		csdev->path = NULL;
 }
 EXPORT_SYMBOL_GPL(coresight_disable_source);
 
