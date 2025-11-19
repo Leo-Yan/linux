c345364ea906c63a26fcde14beff4e20be3c7ab8
Leo Yan <leo.yan@arm.com>
2025-11-19 15:49:04 +0000

    coresight: etm4x: Remove redundant condition checks in save and restore
    Since a dedicated callback is now used to determine whether context save
    and restore operations are needed, performing the same check within the
    save and restore callbacks is redundant.
    
    The save and restore flows currently use two-level functions: the first
    level handles the condition check, while the second level performs the
    low level operations. As the checks are no longer necessary, simplify
    the logic into single-level functions.
    
    Reviewed-by: Mike Leach <mike.leach@linaro.org>
    Tested-by: James Clark <james.clark@linaro.org>
    Signed-off-by: Leo Yan <leo.yan@arm.com>

--- drivers/hwtracing/coresight/coresight-etm4x-core.c
+++ drivers/hwtracing/coresight/coresight-etm4x-core.c
@@ -1876,11 +1876,11 @@ static bool etm4_need_save_restore_context(struct coresight_device *csdev)
 	return false;
 }
 
-static int __etm4_cpu_save(struct etmv4_drvdata *drvdata)
+static int etm4_cpu_save(struct coresight_device *csdev)
 {
 	int i, ret = 0;
 	struct etmv4_save_state *state;
-	struct coresight_device *csdev = drvdata->csdev;
+	struct etmv4_drvdata *drvdata = dev_get_drvdata(csdev->dev.parent);
 	struct csdev_access *csa;
 	struct device *etm_dev;
 
@@ -2018,26 +2018,10 @@ out:
 	return ret;
 }
 
-static int etm4_cpu_save(struct coresight_device *csdev)
-{
-	struct etmv4_drvdata *drvdata = dev_get_drvdata(csdev->dev.parent);
-	int ret = 0;
-
-	if (pm_save_enable != PARAM_PM_SAVE_SELF_HOSTED)
-		return 0;
-
-	/*
-	 * Save and restore the ETM Trace registers only if
-	 * the ETM is active.
-	 */
-	if (coresight_get_mode(drvdata->csdev))
-		ret = __etm4_cpu_save(drvdata);
-	return ret;
-}
-
-static void __etm4_cpu_restore(struct etmv4_drvdata *drvdata)
+static void etm4_cpu_restore(struct coresight_device *csdev)
 {
 	int i;
+	struct etmv4_drvdata *drvdata = dev_get_drvdata(csdev->dev.parent);
 	struct etmv4_save_state *state = drvdata->save_state;
 	struct csdev_access *csa = &drvdata->csdev->access;
 
@@ -2134,17 +2118,6 @@ static void __etm4_cpu_restore(struct etmv4_drvdata *drvdata)
 		etm4_enable_trace_unit(drvdata);
 
 	etm4_cs_lock(drvdata, csa);
-}
-
-static void etm4_cpu_restore(struct coresight_device *csdev)
-{
-	struct etmv4_drvdata *drvdata = dev_get_drvdata(csdev->dev.parent);
-
-	if (pm_save_enable != PARAM_PM_SAVE_SELF_HOSTED)
-		return;
-
-	if (coresight_get_mode(drvdata->csdev))
-		__etm4_cpu_restore(drvdata);
 }
 
 static const struct coresight_ops etm4_cs_ops = {
