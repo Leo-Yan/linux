3bb59ed2fa17f75b19af9585d236e8774787c0ec
Leo Yan <leo.yan@arm.com>
2025-11-24 14:55:18 +0000

    coresight: trbe: Refactor clear status
    Signed-off-by: Leo Yan <leo.yan@arm.com>

--- drivers/hwtracing/coresight/coresight-trbe.c
+++ drivers/hwtracing/coresight/coresight-trbe.c
@@ -618,7 +618,6 @@ static void trbe_enable_hw(struct trbe_buf *buf)
 	WARN_ON(buf->trbe_write < buf->trbe_hw_base);
 	WARN_ON(buf->trbe_write >= buf->trbe_limit);
 
-	clr_trbe_status();
 	set_trbe_base_pointer(buf->trbe_hw_base);
 	set_trbe_write_pointer(buf->trbe_write);
 
@@ -847,6 +846,8 @@ static unsigned long arm_trbe_update_buffer(struct coresight_device *csdev,
 	}
 
 	act = trbe_get_fault_act(handle, status);
+	if (act != TRBE_FAULT_ACT_SPURIOUS)
+		clr_trbe_status();
 
 	if (act == TRBE_FAULT_ACT_WRAP) {
 		trbe_report_wrap_event(handle);
@@ -1036,6 +1037,10 @@ static int arm_trbe_disable(struct coresight_device *csdev)
 		return -EINVAL;
 
 	trbe_drain_and_disable_local(cpudata);
+	clr_trbe_irq();
+	clr_trbe_status();
+	isb();
+
 	buf->cpudata = NULL;
 	cpudata->buf = NULL;
 	cpudata->mode = CS_MODE_DISABLED;
@@ -1152,6 +1157,11 @@ static irqreturn_t arm_trbe_irq_handler(int irq, void *dev)
 		return IRQ_NONE;
 
 	act = trbe_get_fault_act(handle, status);
+
+	/* Clear status register .exp for spurious IRQ for continous state */
+	if (act != TRBE_FAULT_ACT_SPURIOUS)
+		clr_trbe_status();
+
 	switch (act) {
 	case TRBE_FAULT_ACT_WRAP:
 		truncated = !!trbe_handle_overflow(handle);
